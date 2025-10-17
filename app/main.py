d85e893b599166fa013251fb60b8401cf215ed4d
Christopher Pantelli <chris@cptli.com>
2025-10-17 13:47:42 +0100

    Added generate collection form

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 import os
 from fastapi import Depends, FastAPI, HTTPException, Request
-from supabase import Client, create_client
+from supabase import Client
+from app.services.generate_collection_form import generate_collection_form_pdf
 from .services.generate_release_form import generate_release_form_pdf
 from .helpers.supabase import superset_client
 from ulid import ULID
+
 app = FastAPI()
 
 @app.get("/")
@@ -31,6 +33,58 @@ async def generate_release_form(report_id: str, request: Request, supabase: Clie
         report_body = response.data[0]["body"]
 
         pdf_bytes = generate_release_form_pdf(report_body, storage_company)
+
+        upload_response = supabase.storage.from_("generated-reports").upload(
+            path=f"release-forms/{report_id}.pdf",
+            file=pdf_bytes,
+            file_options={"content-type": "application/pdf", "upsert": "true"}
+        )
+
+        if not upload_response:
+            supabase.table('tblbackgroundtasks').update({ 'status': 'failed', 'message': 'Failed to upload PDF' }).eq('id', bg_task_id).execute()
+            raise HTTPException(status_code=500, detail="Failed to upload PDF")
+
+        public_url = (
+            f"{os.environ.get('SUPABASE_URL')}/storage/v1/object/public/"
+            f"{upload_response.full_path}"
+        )
+
+        update_response = supabase.table("tblreports").update({
+            "generated_pdf": public_url
+        }).eq("id", report_id).execute()
+
+        if not update_response.data:
+            supabase.table('tblbackgroundtasks').update({ 'status': 'failed', 'message': 'Failed to update report record' }).eq('id', bg_task_id).execute()
+            raise HTTPException(status_code=500, detail="Failed to update report record")
+
+        supabase.table('tblbackgroundtasks').update({ 'status': 'success', 'message': 'Report Generated' }).eq('id', bg_task_id).execute()
+        return {"status": "success", "url": public_url}
+
+    except HTTPException as e:
+        supabase.table('tblbackgroundtasks').update({ 'status': 'failed', 'message': e }).eq('id', bg_task_id).execute()
+        raise e
+
+    except Exception as e:
+        supabase.table('tblbackgroundtasks').update({ 'status': 'failed', 'message': e }).eq('id', bg_task_id).execute()
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@app.post("/reports/collection-form/{report_id}")
+async def generate_collection_form(report_id: str, request: Request, supabase: Client = Depends(superset_client)):
+    background_task = supabase.table('tblbackgroundtasks').insert({ 'id': str(ULID()), 'status': 'generating', 'type': 'collection-form', 'type_id': report_id, 'message': None }).execute()
+    bg_task_id = background_task.data[0]['id']
+    
+    data = await request.json()
+    
+    try:
+        response = supabase.table("tblreports").select("*").eq("id", report_id).execute()
+        if not response.data:
+            supabase.table('tblbackgroundtasks').update({ 'status': 'failed', 'message': 'Report not found' }).eq('id', bg_task_id).execute()
+            raise HTTPException(status_code=404, detail="Report not found")
+
+        transport_company = data.get("transport_company")
+        report_body = response.data[0]["body"]
+
+        pdf_bytes = generate_collection_form_pdf(report_body, transport_company)
 
         upload_response = supabase.storage.from_("generated-reports").upload(
             path=f"release-forms/{report_id}.pdf",

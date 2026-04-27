5c0f746173ce7d84c94492c26a9364bbb8b44872
Christopher Pantelli <chris@cptli.com>
2026-04-27 20:52:41 +0100

    Changed from ANON to SERVICE_ROLE

--- app/helpers/supabase.py
+++ app/helpers/supabase.py
@@ -3,9 +3,9 @@ from supabase import create_client, Client
 
 def supabase_client() -> Client:
     url: str = os.environ.get("SUPABASE_URL")
-    key: str = os.environ.get("SUPABASE_ANON_KEY")
+    key: str = os.environ.get("SUPABASE_SERVICE_ROLE_KEY")
     
     if not url or not key:
-        raise ValueError("Missing SUPABASE_URL or SUPABASE_ANON_KEY enviroment variables")
+        raise ValueError("Missing SUPABASE_URL or SUPABASE_SERVICE_ROLE_KEY enviroment variables")
     
     return create_client(url, key)
